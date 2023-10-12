ff595f7ea046e7398fa98164fe4268b84a072344
Abu Suraj <abusurajbd@gmail.com>
2023-10-13 02:37:19 +0600

    add login UI

--- src/app/account/login/login.component.ts
+++ src/app/account/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { faEye, faEyeSlash,  } from '@fortawesome/free-solid-svg-icons';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.less']
 })
 export class LoginComponent {
+  faEye = faEye;
+  faEyeSlash = faEyeSlash;
+  loginForm: FormGroup;
+  showPassword: boolean = false;
+  loginError!: string;
+
+  constructor(private fb: FormBuilder) {
+    this.loginForm = this.fb.group({
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  onSubmit() {
+    // Handle form submission, including validation
+  }
+
+  togglePasswordVisibility() {
+    this.showPassword = !this.showPassword;
+  }
+
+  handleGoogleSignin() {
+    // Implement Google login logic here
+  }
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,7 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeadersComponent } from './common-components/headers/headers.component';
 import { FooterComponent } from './common-components/footer/footer.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { FooterComponent } from './common-components/footer/footer.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FontAwesomeModule
   ],
   // exports: [ HeadersComponent,
   //   FooterComponent],
