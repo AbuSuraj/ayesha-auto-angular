ad7208f09168aee2efba452fbac1f77daf3d34ae
Abu Suraj <abusurajbd@gmail.com>
2023-10-26 04:42:01 +0600

    add user name in the header with rxjs operators

--- src/app/common-components/headers/headers.component.ts
+++ src/app/common-components/headers/headers.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { AuthService } from 'src/app/shared/services/auth/auth.service';
 import { ToastrService } from 'ngx-toastr';
+import { UsersService } from 'src/app/shared/services/service-proxies/users/users.service';
+import { switchMap } from 'rxjs';
 @Component({
   selector: 'app-headers',
   templateUrl: './headers.component.html',
@@ -11,18 +13,21 @@ export class HeadersComponent implements OnInit {
   user: any; 
   menuItems: any; 
   activeRoute: ActivatedRoute;
-
-  constructor(private route: ActivatedRoute, private authService: AuthService,  private tost: ToastrService) {
+  loggedInUser:any;
+  constructor(private route: ActivatedRoute, private authService: AuthService,  private tost: ToastrService, private usersService: UsersService) {
     this.activeRoute = route;
    
   }
 
   ngOnInit(): void {
-     this.authService.userDataSubject.subscribe(user => {
-      this.user = user
-      console.log('user', this.user);
-
-    })
+    this.authService.userDataSubject.pipe(
+      switchMap(user => {
+        this.user = user;
+        return this.usersService.getLoggedInUserInfo(this.user);
+      })
+    ).subscribe(loggedInUser => {
+      this.loggedInUser = loggedInUser;
+    });
   }
 
   isActive(route: string): boolean {
